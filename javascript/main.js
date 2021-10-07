260430255a48942a7439ba240b551005ebf14336
Gabriel <gabrielmedsilva@outlook.com>
2021-10-06 23:01:11 -0300

    primeira forma - var

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,21 @@
-alert('olá mundo')
+// Nesse arquivo será feito o estudo da diferença entre as formas
+// de se declarar variáveis no javascript
+
+
+// ===============================
+// ============= var =============
+// ===============================
+
+
+// Essa é a primeira forma de se declarar uma variável, utilizando var
+var msg = 'olá mundo'
+
+alert(msg)
+
+// Porém é a menos utilizada pois pode ser alterada mesmo de fora, como neste caso
+
+if (1 + 1 == 2) {
+    var message = 'true'
+}
+
+alert (message)
