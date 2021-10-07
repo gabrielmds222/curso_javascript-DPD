60728819b5f586529544ec47fa6a60adc68b0f4e
Gabriel <gabrielmedsilva@outlook.com>
2021-10-07 00:44:59 -0300

    length e console.log

---
+++ strings/main.js
@@ -0,0 +1,29 @@
+// Nesse arquivo será feito o estudo do uso de strings no javascript
+
+// strings são arquivos de texto que são usados entre aspas
+
+const message = 'olá mundo'
+
+// Agora veremos alguns comandos relacionados à strings
+
+
+// ============== length ==============
+
+
+// O comando .length mostra a quantidade de caracteres de uma string incluido os espaços
+
+console.log(message.length) // O comando console.log, mostra o resultado do código no navegador
+
+
+
+
+// ============== Unir strings ==============
+
+
+const nome = 'Paulo'
+const sobrenome = 'Soares'
+
+// Para unir essas duas strings e fazê-las aparecerem em sequência, tem-se alguns métodos
+// como por exemplo:
+
+console.log('Meu nome é ' + nome + ' ' + sobrenome)
