014468a65ec334d79d49cef2ec12a7da62cbd2c2
Gabriel <gabrielmedsilva@outlook.com>
2021-10-07 01:25:36 -0300

    finish numbers

---
+++ number-dados/main.js
@@ -0,0 +1,29 @@
+// Nesse arquivo serão mostrados exemplos de dados numéricos no javascript
+
+
+// =============== NUMBER ===============
+
+// O primeiro tipo de dado é o numérico tradicional
+
+const num = 7;
+
+// É possivel realizar as quatro operações matemáticas dentro do console.log
+
+console.log(num + 3); // adição
+console.log(num - 3); // subtração
+console.log(num * 3); // multiplicação
+console.log(num / 3); // divisão
+
+// É possível transformar um número numa string por exemplo
+
+
+console.log(num.toString())
+
+
+// Também é possível descobrir o tipo de uma variável a partir do comando typeof
+
+
+console.log(typeof num) // Nesse caso o resultado será que num é um number
+console.log(typeof num.toString()) // Aqui, ao adicionar o .toString, o resultado será que num é uma string
+
+
