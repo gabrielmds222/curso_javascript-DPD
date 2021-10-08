7d07c419b56eb4d6c5f0010da92dcd7c656ec4df
Gabriel <gabrielmedsilva@outlook.com>
2021-10-08 00:56:57 -0300

    acessar elementos

--- listas/main.js
+++ listas/main.js
@@ -1,5 +1,30 @@
 // ================== LISTAS ==================
 
+// Uma lista é sempre declarada entre colchetes e permite armazenar e ler vários valores em sequência
+
+const nomes = ["Gabriel", "Fernanda", "Jorge", "Paulo"] // Essa será a lista padrão para os exemplos e, é uma lista apenas de strings
+
+// É possível criar listas com outros tipos de dados como números e booleanos
+
+const listnum = [1, 3, 4, 10]
+const bool = [true, false]
+
+// E agora veremos como manipular listas de alguns modos
+
+
+
+// ================== ACESSAR ELEMENTOS ==================
+
+// Para acessar um elemento dentro de uma lista, é preciso lembrar que o primeiro elemento de uma lista corresponde ao valor 0
+
+const fernanda = nomes[1] // Ou seja, nesse caso, a variável vai acessar o elemento "Fernanda"
+
+console.log(fernanda)
+
+// ================== ADICIONAR ELEMENTOS ==================
+// ================== REMOVER ELEMENTOS ==================
+
+
 // add:
 // .push, .unshift,
 
