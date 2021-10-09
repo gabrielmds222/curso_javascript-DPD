acf368e6f579a8d8102a52a8d5447bde782343e3
Gabriel <gabrielmedsilva@outlook.com>
2021-10-09 00:40:17 -0300

    destructuring

--- Objects/main.js
+++ Objects/main.js
@@ -9,14 +9,19 @@ const person = { // Para declarar um objeto, primeiro é necessário declarar um
     hobbies: ["Dormir", "Jogar videogame", "Ler"],
 };
 
-const nome = person.firstName;
-const sobrenome = person.lastName;
-const idade = person.age;
-const hobbies = person.hobbies;
+// Em cada uma das variáveis abaixo, é pego o valor de uma das propriedades do objeto utilizando a seguinte estrutura:
 
+// const nome = person.firstName;
+// const sobrenome = person.lastName;
+// const idade = person.age;
+// const hobbies = person.hobbies;
 
-console.log(nome)
-console.log(sobrenome)
-console.log(idade)
+// A segunda forma de pegar os valores de um objeto é como o exemplo abaixo:
+//          Essa forma é chamada de DESTRUCTURING
+
+const {firstName, lastName, age, hobbies} = person
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
 console.log(hobbies)
-console.log(hobbies[2])
