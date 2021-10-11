43cf53fde61d71513ea9b016eab53ef7f84bf85b
Gabriel <gabrielmedsilva@outlook.com>
2021-10-11 00:51:00 -0300

    for of

--- Loops/main.js
+++ Loops/main.js
@@ -6,7 +6,7 @@
 // A instrução for cria um loop que consiste em três expressões opcionais, dentro de parênteses e separadas por ponto e vírgula,
 //  seguidas por uma declaração ou uma sequência de declarações executadas em sequência.
 
-for (cont = 0; cont < 10; cont ++) { // A primeira expressão do for declara uma variável com um valor inicial que nesse caso é 0
+for (let cont = 0; cont < 10; cont ++) { // A primeira expressão do for declara uma variável com um valor inicial que nesse caso é 0
     console.log(cont);               // A segunda expressão indica o fim do íntervalo, nesse caso o intervalo é de 0 à 9
 }                                    // A terceira expressão indica que a variável do for vai subir de 1 em 1
 
@@ -15,7 +15,29 @@ for (cont = 0; cont < 10; cont ++) { // A primeira expressão do for declara uma
 
 // já no caso abaixo, o for vai imprimir os números de 10 a 1 em ordem decescente, por conta do valor da terceira expressão. 
 
-for (cont = 10; cont > 0; cont --) { 
+for (let cont = 10; cont > 0; cont --) { 
     console.log(cont);               
 } 
 
+
+
+// ==================== FOR com Listas ====================
+
+// É possível criar um loop dentro de uma lista, por exemplo:
+
+const fruits = ['banana', 'maçã', 'abacaxi', 'laranja'];
+
+// Digamos que o objetivo seja fazer um loop que percorra os valores da lista, ou seja, de 0 a 3 e, mostre o nome das frutas em sequência
+
+for (let i = 0; i < fruits.length; i++) { // Nesse exemplo foi usada a variável 'i', no intervalo de 0 até o tamanho da lista e,
+    console.log(fruits[i]); // com o 'i' entre colchetes, o nome de cada fruta será mostrado em sequência
+}
+
+
+                // ======== FOR OF ========
+
+// Uma forma mais eficiente de executar o loop acima é utilizando a estrutura for of
+
+for (let i of fruits) { // Dessa forma, dizemos que enquanto o 'i' estiver dentro da lista, 
+    console.log(i)      // Deve-se exibir o valor de 'i'
+}
