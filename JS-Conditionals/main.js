3ab27d15dbffcd0ae318e7ab56e0de32786bd8b9
Gabriel <gabrielmedsilva@outlook.com>
2021-10-12 00:05:42 -0300

    else if

--- JS-Conditionals/main.js
+++ JS-Conditionals/main.js
@@ -18,9 +18,24 @@ if (soma === 2) { // Nesse caso se o valor da variável for igual a 2,
 
 const sub = 5 - 3;
 
-if (sub <= 1) { // Nesse caso, o valor do if não será o correto,
+if (sub <= 1) { // Nesse caso, o valor do if não será o correto, visto que a operção da variável deve ser 2.
     console.log('False'); 
 }
-else {  
+else {  // Nesse caso, o else vai aprensetar uma solução para quando a condição do if não for executada.
     console.log('True');
+}
+
+
+// =============== ELSE IF ===============
+
+// Para o caso de existirem mais de duas possibilidades de condições a serem testadas, a melhor solução é utilizar o else if
+
+const sum = 1 + 1;
+
+if (sum === 2) {
+  console.log("Sum is 2!");
+} else if (sum === 3) {  // Sendo assim, tem-se uma terceira possibilidade a ser testada e, podem ser usados quantos else if forem precisos
+  console.log("Sum is 3!");
+} else {
+  console.log("Sum is not 2!");
 }
