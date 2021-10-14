f346335b638e0f7bf458ae38af1f89ee25d90887
Gabriel <gabrielmedsilva@outlook.com>
2021-10-14 20:58:49 -0300

    finish form

--- Form_Validation/main.js
+++ Form_Validation/main.js
@@ -13,19 +13,23 @@ submitButton.addEventListener("click", (event) => {
     const emailValue = emailInput.value;
 
     if (nameValue === "" || emailValue === "") {
-        errorMsg.textContent = "Por favor preencher o campo obrigatório"
-        errorMsg.classList = "error";
+        errorMsg.textContent = "Por favor preencher o campo obrigatório" // O text content permite adicionar uma mensagem por meio do javascript
+        errorMsg.classList = "error"; // O método "classList" acessa classes do arquivo css
 
-        setTimeout(() => {
+        setTimeout(() => { // O "setTimeout" Executa uma ação após um certo período de tempo
             errorMsg.textContent = "";
             errorMsg.classList = "";
-          }, 3000);
+          }, 3000); // O período de tempo dessa função será de 3 segundos
           return;
     }
 
-    const li = document.createElement("li");
+    const li = document.createElement("li"); // O "createElement" nos permite criar novos elementos que não estão no arquivo html
     li.classList = "item";
-    li.innerHTML = `Nome: ${nameValue}<br /> Email: ${emailValue}`;
+    li.innerHTML = `Nome: ${nameValue}<br /> Email: ${emailValue}`; // O "innerHTML" permite escrever em HTML no arquivo javascript
 
-    items.appendChild(li);
+    items.appendChild(li); // O método "appendChild" vai adicionar um elemento filho ao elemento principal
+
+
+    nameInput.value = "";
+    emailInput.value = "";
 });
