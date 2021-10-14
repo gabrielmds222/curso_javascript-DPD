5f13d55c1da10bb5174ca50c97ade606c415b8fa
Gabriel <gabrielmedsilva@outlook.com>
2021-10-13 23:44:42 -0300

    arrow again

--- Functions/main.js
+++ Functions/main.js
@@ -5,3 +5,10 @@ function sum(a, b = 10) {
   const sumArrow = (a, b = 10) => {
     return a + b;
   };
+
+  // Mesma coisa que acima
+const sumArrow2 = (a, b = 10) => a + b;
+
+const sumValue = sumArrow2(2);
+
+console.log(sumValue);
