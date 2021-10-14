a1f10fb5b4bf41acdc20eee9372c20951aa67a90
Gabriel <gabrielmedsilva@outlook.com>
2021-10-14 01:36:01 -0300

    finish POO

--- Programação_Orientada_A_Objetos/main.js
+++ Programação_Orientada_A_Objetos/main.js
@@ -21,3 +21,37 @@ const person = new Person("Gabriel", "Medeiros"); // Aqui foi declarada uma vari
 console.log(person);
 person.umMetodoQualquer();
 Person.speak(); // Como speak é um método estático, ele é executado junto à classe.
+
+
+
+// ==============  HERANÇA DE CLASSE  ==============
+
+// É possível criar uma classe que herde as informações de outras classes da seguinte forma:
+
+class Animal { // Primeiro temos uma classe normal, que  será a classe "pai".
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} made some noise!`); // O objetivo dessa classe é executar a mensagem "(animal) fez um barulho."
+    }
+  }
+
+  const animal = new Animal("Simba"); // Essa variável declara o valor do parâmetro name, ou seja, o nome do animal é simba
+  animal.speak();
+
+// Mas como a classe Animal é muito ampla, se o objetivo for algo mais específico, é possível fazer da seguinte forma:
+
+  class Dog extends Animal { // Foi criada uma classe Dog e, usada a propriedade extends, que permite essa classe utilizar todas as propriedades de outra classe
+    constructor(name) {
+      super(name); // A oalavra-chave super é uma forma de chamar o parâmetro utilizado na classe principal
+    }
+  
+    speak() {
+      console.log(`${this.name} barked!`); // Aqui o método speak foi sobrescrito para se adaptar caso o animal seja um cachorro
+    }
+  }
+  
+  const dog = new Dog("Bob");
+  dog.speak();
